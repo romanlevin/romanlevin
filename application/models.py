f2a4e61bf59de192f8bd41f3450fbacc8461953d
Roman Levin <romanlevin@gmail.com>
2012-06-17 19:03:12 +0300

    more init

--- application/models.py
+++ application/models.py
@@ -15,3 +15,16 @@ class ExampleModel(db.Model):
     example_description = db.TextProperty(required=True)
     added_by = db.UserProperty()
     timestamp = db.DateTimeProperty(auto_now_add=True)
+
+
+class Post(db.Model):
+    title = db.StringProperty(required=True)
+    author = db.UserProperty(required=True)
+    body = db.TextProperty(required=True)
+    timestamp = db.DateTimeProperty(auto_now_add=True)
+
+
+class Project(db.Model):
+    name = db.StringProperty(required=True)
+    author = db.UserProperty(required=True)
+    timestamp = db.DateTimeProperty(auto_now_add=True)
